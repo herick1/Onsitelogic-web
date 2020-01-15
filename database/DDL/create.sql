162e3f57564feaf2bd77a9d5c36a57b70345bec6
herick1 <herick200@gmail.com>
2020-01-15 10:32:04 -0400

    fix: arreglando show en participante

--- database/DDL/create.sql
+++ database/DDL/create.sql
@@ -1,50 +1,50 @@
-create table Lugar(
-  id numeric(10) unique,
-  nombre varchar(40) not null,
-  tipo varchar(30) not null,
-  fk_Lugar numeric(10),
-  constraint pk_idLugar primary key(id),
-  constraint fk_Lugar_id FOREIGN KEY (id) REFERENCES Lugar (id)
-);
+    create table Lugar(
+      id integer unique,
+      nombre varchar(40) not null,
+      tipo varchar(30) not null,
+      fk_Lugar integer,
+      constraint pk_idLugar primary key(id),
+      constraint fk_Lugar_id FOREIGN KEY (fk_Lugar) REFERENCES Lugar (id)
+    );
 
-create table Participante(
-   id integer,
-   cedula numeric(10),
-   email varchar(200),
-   pimer_nombre varchar(30),
-   segundo_nombre varchar(30),
-   primer_apellido varchar(60),
-   segundo_apellido varchar(60),
-   fecha_de_nacimiento date,
-   telefono varchar(30),
-   tipo varchar(20),
-   fk_lugar integer,
-   constraint pk_idUsuario primary key(id),
-   constraint fk_parlugar foreign key(fk_lugar) references Lugar(id),
-   constraint CK_tipo check(tipo in('Visitante','Exponente','Asesor', 'Otros'))
-);
+    create table Participante(
+       id integer unique AUTO_INCREMENT,
+       cedula numeric(10),
+       email varchar(200),
+       pimer_nombre varchar(30),
+       segundo_nombre varchar(30),
+       primer_apellido varchar(60),
+       segundo_apellido varchar(60),
+       fecha_de_nacimiento date,
+       telefono varchar(30),
+       tipo varchar(20),
+       fk_lugar integer,
+       constraint pk_idUsuario primary key(id),
+       constraint fk_parlugar foreign key(fk_lugar) references Lugar(id),
+       constraint CK_tipo check(tipo in('Visitante','Exponente','Asesor', 'Otros'))
+    );
 
-create table Evento(
-   id integer,
-   tipo varchar(20),
-   nombre varchar(60),
-   cantidad_de_personas numeric(5),
-   fecha_inicio date,
-   fecha_fin date,
-   fk_lugar integer,
-   constraint pk_idEvento primary key(id),
-   constraint fk_parlugar foreign key(fk_lugar) references Lugar(id)
-);
+    create table Evento(
+       id integer unique AUTO_INCREMENT,
+       tipo varchar(20),
+       nombre varchar(60),
+       cantidad_de_personas numeric(5),
+       fecha_inicio date,
+       fecha_fin date,
+       fk_lugar integer,
+       constraint pk_idEvento primary key(id),
+       constraint fk_Evenlugar foreign key(fk_lugar) references Lugar(id)
+    );
 
-create table Historial_Usuario_Evento(
-   id integer,
-   asistencia int NOT NULL,
-  fecha_inicio date,
-   fecha_fin date,
-   fk_participante integer,
-   fk_evento integer,
-   constraint pk_id_historial primary key(id),
-   constraint fk_particiHistorial foreign key(fk_participante) references Participante(id),
-   constraint fk_eventHistorial foreign key(fk_evento) references Evento(id),
-   constraint CK_boo_asistencia CHECK (asistencia IN (0, 1))
-);
+    create table Historial_Usuario_Evento(
+       id integer,
+       asistencia int NOT NULL,
+      fecha_inicio date,
+       fecha_fin date,
+       fk_participante integer,
+       fk_evento integer,
+       constraint pk_id_historial primary key(id),
+       constraint fk_particiHistorial foreign key(fk_participante) references Participante(id),
+       constraint fk_eventHistorial foreign key(fk_evento) references Evento(id),
+       constraint CK_boo_asistencia CHECK (asistencia IN (0, 1))
+    );
--- resources/views/participantes/show.blade.php
+++ resources/views/participantes/show.blade.php
@@ -5,6 +5,7 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2">Detalles del participante</h1>
     </div>
+    <div class=" col-sm-8 col-12 card text-left p-5" style="margin-left: 15%">
 	    <br>
 	   	<br>
         <strong>Cédula de identidad:&nbsp;</strong> <i>{{$participante->cedula}}</i><br>
@@ -12,9 +13,10 @@
         <strong>primer nombre :&nbsp;</strong> <i>{{$participante->pimer_nombre}}</i><br>
 	    <br>
 	   	<br>
-    <a href="{{route('participantes.index')}}" class="btn btn-outline-info" role="button">
-        Volver
-    </a>
+        <a href="{{route('participantes.index')}}" class="btn btn-outline-info" role="button">
+            Volver
+        </a>
+    </div>
     	<br>
     	<br>
 @stop;
